5a5d5a9bf61631ea52f231a08708e3cf3922511d
Mac <mac@eduroam-057-085.scc.uni-weimar.de>
2016-07-24 15:27:26 +0200

    Setup data type CreativeWork as a placeholder for a repository

--- app/models/IdentifiedResourceTypes.java
+++ app/models/IdentifiedResourceTypes.java
@@ -1,14 +1,14 @@
 package models;
 
 public enum IdentifiedResourceTypes {
-	
-	ORGANIZATION("Organization"), EVENT("Event"), PERSON("Person");
+
+	ORGANIZATION("Organization"), EVENT("Event"), PERSON("Person"), CREATIVEWORK("CreativeWork");
 	private final String mType;
-	
+
 	private IdentifiedResourceTypes(final String aResourceType){
 		mType = aResourceType;
 	}
-	
+
 	public String toString(){
 		return mType;
 	}
